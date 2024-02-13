df10df272e7e79be6c7bf38094eace0ec43984b4
marcoimbee <57362927+marcoimbee@users.noreply.github.com>
2024-02-13 16:17:16 +0100

    Add files via upload

---
+++ test/LoginBruteforce.py
@@ -0,0 +1,33 @@
+import requests
+
+cookies = {
+    'PHPSESSID': 'd6b903fgvlnv1mfrenv8s915v2',
+}
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Origin': 'http://localhost',
+    'Pragma': 'no-cache',
+    'Referer': 'http://localhost/login',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+}
+
+data = {
+    'username': 'a@gmail.com',
+    'password': 'Abcde11@lasdsa23',
+}
+while True:
+    response = requests.post('http://localhost/login', cookies=cookies, headers=headers, data=data)
+    print("Status code: ", response.status_code)
